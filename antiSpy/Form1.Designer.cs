885b7e17c34eca0c63a777cd152da053dd72c4c1
Inf <saniainf@gmail.com>
2013-10-22 14:26:37 +0700

    well done

---
+++ antiSpy/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace antiSpy
+{
+    partial class fAntiSpy
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fAntiSpy));
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.btnAuto = new System.Windows.Forms.Button();
+            this.trayMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.manualTrayMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.autoTrayMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeTrayMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnManual = new System.Windows.Forms.Button();
+            this.numTimerPref = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.trayMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numTimerPref)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+            this.trayIcon.Text = "AntiSpy";
+            this.trayIcon.Visible = true;
+            this.trayIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.trayIcon_MouseDoubleClick);
+            // 
+            // lstLog
+            // 
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.Location = new System.Drawing.Point(12, 24);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(191, 420);
+            this.lstLog.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(209, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(648, 420);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // btnAuto
+            // 
+            this.btnAuto.Location = new System.Drawing.Point(12, 469);
+            this.btnAuto.Name = "btnAuto";
+            this.btnAuto.Size = new System.Drawing.Size(98, 23);
+            this.btnAuto.TabIndex = 2;
+            this.btnAuto.Text = "Автоматически";
+            this.btnAuto.UseVisualStyleBackColor = true;
+            this.btnAuto.Click += new System.EventHandler(this.switchAuto);
+            // 
+            // trayMenu
+            // 
+            this.trayMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manualTrayMenuItem,
+            this.autoTrayMenuItem,
+            this.closeTrayMenuItem});
+            this.trayMenu.Name = "trayMenu";
+            this.trayMenu.Size = new System.Drawing.Size(158, 92);
+            // 
+            // manualTrayMenuItem
+            // 
+            this.manualTrayMenuItem.Name = "manualTrayMenuItem";
+            this.manualTrayMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.manualTrayMenuItem.Text = "в ручную";
+            this.manualTrayMenuItem.Click += new System.EventHandler(this.switchManual);
+            // 
+            // autoTrayMenuItem
+            // 
+            this.autoTrayMenuItem.Name = "autoTrayMenuItem";
+            this.autoTrayMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.autoTrayMenuItem.Text = "автоматически";
+            this.autoTrayMenuItem.Click += new System.EventHandler(this.switchAuto);
+            // 
+            // closeTrayMenuItem
+            // 
+            this.closeTrayMenuItem.Name = "closeTrayMenuItem";
+            this.closeTrayMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.closeTrayMenuItem.Text = "выход";
+            this.closeTrayMenuItem.Click += new System.EventHandler(this.closeTrayMenuItem_Click);
+            // 
+            // btnManual
+            // 
+            this.btnManual.Location = new System.Drawing.Point(116, 469);
+            this.btnManual.Name = "btnManual";
+            this.btnManual.Size = new System.Drawing.Size(87, 23);
+            this.btnManual.TabIndex = 3;
+            this.btnManual.Text = "В ручную";
+            this.btnManual.UseVisualStyleBackColor = true;
+            this.btnManual.Click += new System.EventHandler(this.switchManual);
+            // 
+            // numTimerPref
+            // 
+            this.numTimerPref.InterceptArrowKeys = false;
+            this.numTimerPref.Location = new System.Drawing.Point(209, 469);
+            this.numTimerPref.Maximum = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
+            this.numTimerPref.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numTimerPref.Name = "numTimerPref";
+            this.numTimerPref.Size = new System.Drawing.Size(120, 20);
+            this.numTimerPref.TabIndex = 5;
+            this.numTimerPref.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numTimerPref.ValueChanged += new System.EventHandler(this.numTimerPref_ValueChanged);
+            // 
+            // fAntiSpy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 501);
+            this.Controls.Add(this.numTimerPref);
+            this.Controls.Add(this.btnManual);
+            this.Controls.Add(this.btnAuto);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lstLog);
+            this.Name = "fAntiSpy";
+            this.Text = "AntiSpy";
+            this.Resize += new System.EventHandler(this.fAntiSpy_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.trayMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numTimerPref)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.ListBox lstLog;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button btnAuto;
+        private System.Windows.Forms.ContextMenuStrip trayMenu;
+        private System.Windows.Forms.ToolStripMenuItem manualTrayMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem autoTrayMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeTrayMenuItem;
+        private System.Windows.Forms.Button btnManual;
+        private System.Windows.Forms.NumericUpDown numTimerPref;
+    }
+}
+
---
+++ antiSpy/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace antiSpy
+{
+    public partial class fAntiSpy : Form
+    {
+        Bitmap bmpScreenShot = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                          Screen.PrimaryScreen.Bounds.Height,
+                          System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+        Graphics gScreenShot;
+
+        string pathManual;
+        string pathAuto;
+        string pathLan;
+        bool mode; //true - Auto, false - Manual
+
+        public fAntiSpy()
+        {
+            InitializeComponent();
+
+            gScreenShot = Graphics.FromImage(bmpScreenShot);
+
+            //TODO заменить на настройки
+            pathManual = Properties.Settings.Default.pathManual;
+            pathAuto = Properties.Settings.Default.pathAuto;
+            pathLan = Properties.Settings.Default.pathLan;
+            mode = Properties.Settings.Default.mode;
+            timer.Interval = Properties.Settings.Default.timer * 1000;
+            numTimerPref.Value = Properties.Settings.Default.timer;
+
+            //TODO заменить на настройки
+            if (mode)
+            {
+                autoTrayMenuItem.Checked = true;
+                btnAuto.Enabled = false;
+            }
+            else
+            {
+                manualTrayMenuItem.Checked = true;
+                btnManual.Enabled = false;
+            }
+
+            //событие нажатия кнопки
+            HotKeyManager.RegisterHotKey(Keys.End, KeyModifiers.Alt);
+            HotKeyManager.HotKeyPressed += new EventHandler<HotKeyEventArgs>(gkh_KeyDown);
+
+            //сделать иконку невидимой
+            trayIcon.Visible = true;
+            //меню иконки в трее
+            trayIcon.ContextMenuStrip = trayMenu;
+
+            //вкл таймер
+            timer.Enabled = true;
+        }
+
+        private void trayIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            trayIcon.Visible = false;
+            this.ShowInTaskbar = true;
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void fAntiSpy_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+                trayIcon.Visible = true;
+            }
+        }
+
+        //перехват кнопки
+        private void gkh_KeyDown(object sender, HotKeyEventArgs e)
+        {
+            if (!mode)
+            {
+                makeScrShot();
+
+                //сохранить картинку с именем текущей даты
+                bmpScreenShot.Save(pathManual + DateTime.Now.ToString("yyMMdd_HHmmss") + ".jpg", ImageFormat.Jpeg);
+                lstLog.Items.Add("press btn");
+            }
+        }
+
+        //тик таймера
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            lstLog.Items.Add("tic");
+
+            if (mode)
+            {
+                makeScrShot();
+
+                //сохранить картинку с именем компа 
+                bmpScreenShot.Save(pathAuto + Environment.MachineName + ".jpg", ImageFormat.Jpeg);
+            }
+            //переместить файл
+            fileMove();
+        }
+
+        //сделать новый скрин в bmpScreenShot
+        private void makeScrShot()
+        {
+            gScreenShot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+               Screen.PrimaryScreen.Bounds.Y,
+               0, 0,
+               Screen.PrimaryScreen.Bounds.Size,
+               CopyPixelOperation.SourceCopy);
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.Image = bmpScreenShot;
+        }
+
+        //переместить файл
+        private void fileMove()
+        {
+            if (mode)
+            {
+                if (File.Exists(pathAuto + Environment.MachineName + ".jpg"))
+                {
+                    File.Delete(pathLan + Environment.MachineName + ".jpg");
+                    File.Move(pathAuto + Environment.MachineName + ".jpg", pathLan + Environment.MachineName + ".jpg");
+                    lstLog.Items.Add("move auto");
+                }
+            }
+            else
+            {
+                string[] allFiles = Directory.GetFiles(pathManual);
+                if (allFiles.GetLength(0) > 0)
+                {
+                    File.Delete(pathLan + Environment.MachineName + ".jpg");
+                    File.Move(allFiles[0], pathLan + Environment.MachineName + ".jpg");
+                    lstLog.Items.Add("move manually");
+                }
+            }
+        }
+
+        # region mode switch
+
+        private void closeTrayMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void switchAuto(object sender, EventArgs e)
+        {
+            if (!mode)
+            {
+                manualTrayMenuItem.Checked = false;
+                autoTrayMenuItem.Checked = true;
+                btnAuto.Enabled = false;
+                btnManual.Enabled = true;
+                mode = true;
+                Properties.Settings.Default.mode = true;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void switchManual(object sender, EventArgs e)
+        {
+            if (mode)
+            {
+                manualTrayMenuItem.Checked = true;
+                autoTrayMenuItem.Checked = false;
+                btnAuto.Enabled = true;
+                btnManual.Enabled = false;
+                mode = false;
+                Properties.Settings.Default.mode = false;
+                Properties.Settings.Default.Save();
+            }
+        }
+        # endregion
+
+        private void numTimerPref_ValueChanged(object sender, EventArgs e)
+        {
+            timer.Interval = (int)numTimerPref.Value * 1000;
+            Properties.Settings.Default.timer = (int)numTimerPref.Value;
+            Properties.Settings.Default.Save();
+        }
+    }
+
+    #region hotkey
+    public class HotKeyManager
+    {
+        public static event EventHandler<HotKeyEventArgs> HotKeyPressed;
+
+        public static int RegisterHotKey(Keys key, KeyModifiers modifiers)
+        {
+            int id = System.Threading.Interlocked.Increment(ref _id);
+            RegisterHotKey(_wnd.Handle, id, (uint)modifiers, (uint)key);
+            return id;
+        }
+
+        public static bool UnregisterHotKey(int id)
+        {
+            return UnregisterHotKey(_wnd.Handle, id);
+        }
+
+        protected static void OnHotKeyPressed(HotKeyEventArgs e)
+        {
+            if (HotKeyManager.HotKeyPressed != null)
+            {
+                HotKeyManager.HotKeyPressed(null, e);
+            }
+        }
+
+        private static MessageWindow _wnd = new MessageWindow();
+
+        private class MessageWindow : Form
+        {
+            protected override void WndProc(ref Message m)
+            {
+                if (m.Msg == WM_HOTKEY)
+                {
+                    HotKeyEventArgs e = new HotKeyEventArgs(m.LParam);
+                    HotKeyManager.OnHotKeyPressed(e);
+                }
+
+                base.WndProc(ref m);
+            }
+
+            private const int WM_HOTKEY = 0x312;
+        }
+
+        [DllImport("user32")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private static int _id = 0;
+    }
+
+
+    public class HotKeyEventArgs : EventArgs
+    {
+        public readonly Keys Key;
+        public readonly KeyModifiers Modifiers;
+
+        public HotKeyEventArgs(Keys key, KeyModifiers modifiers)
+        {
+            this.Key = key;
+            this.Modifiers = modifiers;
+        }
+
+        public HotKeyEventArgs(IntPtr hotKeyParam)
+        {
+            uint param = (uint)hotKeyParam.ToInt64();
+            Key = (Keys)((param & 0xffff0000) >> 16);
+            Modifiers = (KeyModifiers)(param & 0x0000ffff);
+        }
+    }
+
+    [Flags]
+    public enum KeyModifiers
+    {
+        Alt = 1,
+        Control = 2,
+        Shift = 4,
+        Windows = 8,
+        NoRepeat = 0x4000
+    }
+    #endregion
+}
